bd6bc39afd671b305e46e01e14cda478d6fb5fb8
Orçun Çolak <orcun.colak@hazelcast.com>
2024-05-22 12:34:08 +0300

    Files.lines example

--- src/main/java/com/colak/files/FilesLinesTest.java
+++ src/main/java/com/colak/files/FilesLinesTest.java
@@ -3,10 +3,13 @@ package com.colak.files;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -14,10 +17,21 @@ import java.util.stream.Stream;
 @UtilityClass
 class FilesLinesTest {
 
-    public static void main() throws Exception {
-        URI uri = FilesLinesTest.class.getClassLoader().getResource("test.txt").toURI();
+    public static void main() throws IOException, URISyntaxException {
+        URI uri = Objects.requireNonNull(FilesLinesTest.class.getClassLoader().getResource("test.txt")).toURI();
         Path file = Paths.get(uri);
 
+        printFile(file);
+        findLine(file);
+    }
+
+    private static void printFile(Path file) throws IOException {
+        try (Stream<String> lines = Files.lines(file)) {
+            lines.forEach(log::info);
+        }
+    }
+
+    private static void findLine(Path file) throws IOException {
         Optional<String> line;
         try (Stream<String> lines = Files.lines(file)) {
             line = lines.filter(l -> l.contains("line2"))
