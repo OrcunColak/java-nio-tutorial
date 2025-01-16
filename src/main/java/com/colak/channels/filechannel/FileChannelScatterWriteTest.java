23fc8141805398936ca4935998c650de338a0e54
Orçun Çolak <orcuncolak@yahoo.com>
2025-01-16 08:07:08 +0300

    Create FileChannelScatterWriteTest.java

---
+++ src/main/java/com/colak/channels/filechannel/FileChannelScatterWriteTest.java
@@ -0,0 +1,28 @@
+package com.colak.channels.filechannel;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+@Slf4j
+public class FileChannelScatterWriteTest {
+
+    public static void main(String[] args) {
+
+      ByteBuffer header = ByteBuffer.allocate(10); 
+      ByteBuffer body = ByteBuffer.allocate(1024); 
+      ByteBuffer footer = ByteBuffer.allocate(10); 
+      ByteBuffer[] buffers = { header, body, footer };
+      
+      try (FileChannel channel = new FileOutputStream("example.txt").getChannel()) {
+        channel.write(buffers);
+          
+      } catch (IOException exception) {
+        log.error("Exception : ", exception);
+      }
+    }
+}
